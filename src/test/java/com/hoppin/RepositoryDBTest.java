24b4a16ee46c68a3d9cf9b80339f5a7122ed0db0
jinsung <viviamm7@gmail.com>
2026-04-21 22:41:46 +0900

    docs : Test 코드 수정

--- src/test/java/com/hoppin/RepositoryDBTest.java
+++ src/test/java/com/hoppin/RepositoryDBTest.java
@@ -13,11 +13,13 @@ import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.time.LocalDate;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+@ActiveProfiles("test")
 @SpringBootTest
 @Transactional
 class RepositoryDBTest {
