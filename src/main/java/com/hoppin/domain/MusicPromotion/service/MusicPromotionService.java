c60eb1f12f9e55e350f58a1379153a0c6773b669
jinsung <viviamm7@gmail.com>
2026-05-07 03:13:46 +0900

    fix : 내 홍보가 아닌데 조회되는 버그 수정

--- src/main/java/com/hoppin/domain/MusicPromotion/service/MusicPromotionService.java
+++ src/main/java/com/hoppin/domain/MusicPromotion/service/MusicPromotionService.java
@@ -99,9 +99,11 @@ public class MusicPromotionService {
     }
 
     @Transactional(readOnly = true)
-    public MusicPromotionDetailResponse getMusicPromotion(Long promotionId) {
+    public MusicPromotionDetailResponse getMusicPromotion(Long musicianId, Long promotionId) {
         MusicPromotion promotion = musicPromotionRepository.findById(promotionId)
                 .orElseThrow(() -> new ResourceNotFoundException("음악 홍보를 찾을 수 없습니다."));
+
+        validateOwnership(musicianId, promotion);
 
         String trackingUrl = trackingLinkRepository.findFirstByPromotionId(promotionId)
                 .map(PromotionTrackingLink::getTrackingUrl)
